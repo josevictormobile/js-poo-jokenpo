f9e5fa365683142977b75ea46419508ac5b39fe7
unknown <josevictormobile@gmail.com>
2021-08-11 06:02:08 -0300

    Inicio do tratamento de entradas

---
+++ interface/tratamento_entrada.js
@@ -0,0 +1,12 @@
+function tratarEntrada(entrada){
+    if(entrada === 'Pe'|| entrada === 'pe'|| entrada === 'Ped'|| entrada === 'ped'|| 
+    entrada === 'Pedr'|| entrada === 'pedr'|| entrada === 'pedra' || entrada === 'Pedra'|| 
+    entrada === 'peda'|| entrada === 'Peda'|| entrada === 'rock'){
+        entrada = 'pedra';
+    }
+    if(entrada === 'Pe'|| entrada === 'pe'|| entrada === 'Ped'|| entrada === 'ped'|| 
+    entrada === 'Pedr'|| entrada === 'pedr'|| entrada === 'pedra' || entrada === 'Pedra'|| 
+    entrada === 'peda'|| entrada === 'Peda'|| entrada === 'rock'){
+        entrada = 'pedra';
+    }
+}
